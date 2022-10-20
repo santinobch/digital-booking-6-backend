47629a7e70dc6491b0fc9e705b46d65621a707d4
Martin Rosas <mlrosas21@hotmail.com>
2022-10-19 21:02:14 -0300

    Initializing Spring project

---
+++ src/main/java/com/example/DigitalBookingBEG6/DigitalBookingBeG6Application.java
@@ -0,0 +1,13 @@
+package com.example.DigitalBookingBEG6;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DigitalBookingBeG6Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DigitalBookingBeG6Application.class, args);
+	}
+
+}
