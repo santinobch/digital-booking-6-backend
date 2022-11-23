17826b1e7c4e15f5d03713ac24bbeda98bf6fac4
Martin Rosas <mlrosas21@hotmail.com>
2022-11-23 20:56:30 -0300

    FIX CORS #2... ?

--- src/main/java/com/example/DigitalBookingBEG6/DigitalBookingBeG6Application.java
+++ src/main/java/com/example/DigitalBookingBEG6/DigitalBookingBeG6Application.java
@@ -15,17 +15,4 @@ public class DigitalBookingBeG6Application {
 	public static void main(String[] args) {
 		SpringApplication.run(DigitalBookingBeG6Application.class, args);
 	}
-
-	@Bean
-	public WebMvcConfigurer corsConfigurer() {
-		return new WebMvcConfigurer() {
-			@Override
-			public void addCorsMappings(CorsRegistry registry) {
-				registry.addMapping("/**")
-						.allowedOrigins("*")
-						.allowedMethods("*")
-						.allowedHeaders("*");
-			}
-		};
-	}
 }
