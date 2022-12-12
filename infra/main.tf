b7c4faf2322279d927ca531ea94a16b517979c92
Oscar Pulido <oscarpu_14@hotmail.com>
2022-12-12 16:32:19 -0500

    infra

--- infra/main.tf
+++ infra/main.tf
@@ -1,6 +1,5 @@
-#postman https://app.getpostman.com/join-team?invite_code=8b745fcf2d1351299a36fff26a6090d8
 provider "aws" {
-  region        = "us-east-2"
+  region        = var.pi_region
   access_key    = var.pi_access_key
   secret_key    = var.pi_secret_key
 }
@@ -17,51 +16,56 @@ data "template_file" "init" {
   template = "${file("${path.module}/scripts/user-data.sh")}"
 }
 
-#ssh -i "llave.pem" ec2-user@ec2-18-188-157-88.us-east-2.compute.amazonaws.com
-#Llave de acceso
 resource "aws_key_pair" "deployer" {
   key_name   = "deployer-key"
-  public_key = "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABgQCzc/69117MBhUWTanreucuZfS/hy3BGTyvLbBvW6tTCxrGNXsG9usVFPZC6ZxmoKdAvPzW86cInHQM4+fB5k0mF98L6ye94VZ+ai8HUyFkeEJsvRtedCVXQrKIywjZohRVZZBFd/jn45bF06idl1uHbw3mnClMAlj79soyFe2L/kXTqDmzzi+PkupS44n2EDwVz9+qEyJ9GIeIpUg0H6NQ0EUKDflt3ryJp2xvRl1TO+j89AAQM9F7S7oor4+jaB4XO2W7riOoUMDjUzKBQShhiaRY2rlj/GHm/QsaA+yMBDz9TjFjjGsBGixRsArfhgGTX/BRkppUr3HnV4zS1sXGMZlftlsqjB4F3AWngxt0ow5bbs+79r6rkAXX2YiEDf/kOe3CKSIIuqKC+KTbEki3espGi+Bl6wVVDbEsrvMEKGK5j3I/Si5Y1hBafXXHny+HNf7uXyJvphvZMMG0HVQm2bfstXNsFGtXs5VvK6K1haOc9xf1uMDpeLiQrqfd6F8= oscar@OscarDraws"
+  public_key = var.pi_public_rsa_key
 }
 
 # EC2 For Backend
 resource "aws_instance" "backend" {
-  ami           = "ami-0beaa649c482330f7"
+  ami           = var.pi_ami_id
   instance_type = "t2.micro"
   key_name      =  aws_key_pair.deployer.key_name
 
   user_data = data.template_file.init.rendered
 
   tags = {
-    Name = "0521ptc8n2-grupo6-backend"
+    Name = var.pi_name_back
   }
 }
 
 #Bucket S3 for Frontend
 resource "aws_s3_bucket" "bucketfrontend" {
-  bucket        = "0521ptc8n2-grupo6-bk-frontend"
+  bucket        = var.pi_bucket_front
+  policy        = file("s3-policy.json")
+
+  website {
+    index_document  = "index.html"
+  }
 
   tags = {
-    Name        = "0521ptc8n2-grupo6-bk-frontend"
+    Name            = var.pi_bucket_front
   }
 }
 
 # S3 For Frontent
 resource "aws_s3_bucket_acl" "frontend" {
   bucket        = aws_s3_bucket.bucketfrontend.id
-  acl           = "private"
+  acl           = "public-read"
 }
 
 #Bucket S3 for Images
 resource "aws_s3_bucket" "bucketimages" {
-  bucket        = "0521ptc8n2-grupo6-bk-images"
+  bucket        = var.pi_bucket_img
+  policy        = file("s3-img-policy.json")
 
   tags = {
-    Name        = "0521ptc8n2-grupo6-bk-images"
+    Name        = var.pi_bucket_img
   }
 }
+
 # S3 For Images
 resource "aws_s3_bucket_acl" "images" {
   bucket        = aws_s3_bucket.bucketimages.id
-  acl           = "private"
+  acl           = "public-read"
 }
